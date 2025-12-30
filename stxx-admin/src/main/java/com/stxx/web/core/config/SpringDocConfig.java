17ef4a8680c4bd11184920e4a309ddd364d0b0d9
wangcc <wangcc1987@139.com>
2025-12-30 10:47:01 +0800

    springdoc优化认证参数

--- stxx-admin/src/main/java/com/stxx/web/core/config/SpringDocConfig.java
+++ stxx-admin/src/main/java/com/stxx/web/core/config/SpringDocConfig.java
@@ -8,9 +8,10 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SpringDocConfig {
@@ -45,21 +46,21 @@ public class SpringDocConfig {
     }
 
     /**
-     * Knife4j 全局 Header 参数补充
-     * 让 Knife4j 的全局参数列表显示 Authorization
+     * Knife4j 全局 Header 参数
      */
     @Bean
-    public OpenApiCustomiser knife4jGlobalHeader() {
-        return openApi -> openApi.getPaths().values().forEach(pathItem ->
-                pathItem.readOperations().forEach(operation ->
-                        operation.addParametersItem(
-                                new io.swagger.v3.oas.models.parameters.Parameter()
-                                        .in("header")
-                                        .name("Authorization")
-                                        .description("Bearer token-xxxx")
-                                        .required(false)
-                        )
-                )
-        );
+    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
+        return openApi -> {
+            // 全局添加鉴权参数
+            if(openApi.getPaths()!=null){
+                openApi.getPaths().forEach((s, pathItem) -> {
+                    // 为所有接口添加鉴权
+                    pathItem.readOperations().forEach(operation -> {
+                        operation.addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION));
+                    });
+                });
+            }
+
+        };
     }
  }
